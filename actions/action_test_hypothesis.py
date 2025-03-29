a1fc56b6c22d95ffbb3c6d3f8f3c7aa8b26b7984
silvererudite <shamima.hossain19@gmail.com>
2025-03-29 19:41:42 +0600

    Add project scaffold with overall flow

---
+++ actions/action_test_hypothesis.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict, List, Text
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionTestHypothesis(Action):
+    def name(self) -> Text:
+        return "action_test_hypothesis"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        pass
